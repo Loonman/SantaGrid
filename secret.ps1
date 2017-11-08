dedfa1fe94fcba9143b4e5d9f9e4988f768e3f1c
Ryan Huard <ryan.huard@live.ca>
2017-11-07 20:02:39 -0700

    Function-ized and added docstring

--- secret.ps1
+++ secret.ps1
@@ -1,8 +1,18 @@
-param($File)
+<#
+.SYNOPSIS
+This is a simple powershell script for creating and sending secret santa pairings over a sendgrid email service
 
-if ($File -eq "" -or $File -eq $null)
+.DESCRIPTION
+This script will take a .csv file containing the columns 'name' and 'email', an output logfile, and an email address to send from. Then it will randomly pair all attendees. The algorithm used guarantees that no one will receive their own name and since all pairings happen at once guarntees random pairings.
+
+.EXAMPLE
+./SecretSanta.ps1 -Signups signups.csv -LogFile out.log -SendAddr SecretSanta@gmail.com
+#>
+param($Signups, $LogFile, $SendAddr)
+
+if (!(Test-Path $signups))
 {
-    write-host "You must supply a file you scrub"
+    write-host "You must supply a signups of signups."
     return
 }
 
@@ -18,36 +28,54 @@ function makePairings ([System.Collections.ArrayList]$attendees)
     $last.match = $first.name
 }
 
-function sendOutEmails([System.Collections.ArrayList]$matches) 
+function sendOutEmails([System.Collections.ArrayList]$matches, $credentials) 
 {
-    $username = "azure_2e52969a583f648d72302eab0139df2b@azure.com"
-    $password = cat .\password.txt | ConvertTo-SecureString
-    $cred = new-object -typename System.Management.Automation.PSCredential `
-            -ArgumentList $username, $password
+    #$username = cat .\user.txt
+    #$password = cat .\password.txt | ConvertTo-SecureString
+    #$cred = new-object -typename System.Management.Automation.PSCredential `
+    #        -ArgumentList $username, $password
     foreach($entry in $matches) 
     {
-        Send-MailMessage -From "SecretSanta@compeclub.com" `
+        Send-MailMessage -From $SendAddr `
         -To $entry.email `
         -Subject "Secret santa pairings!" `
-        -body "Hi $($entry.name). You will be buying for $($entry.match)!" `
+        -body "Hi $($entry.name). You will be gifting for $($entry.match)! Happy Holidays!" `
         -SmtpServer "smtp.sendgrid.net" `
         -port 587 `
-        -credential $cred `
+        -credential $credentials `
         -usessl
     }
 }
 
-$rows = new-object System.Collections.ArrayList
-Import-CSV $File | foreach-object {
-    $hash = (@{Name=$_.name;email=$_.email;match=""})
-    $row = new-object psobject -Property $hash
-    $rows.add($row) > $null
+function GetRandomRows()
+{
+    $rows = new-object System.Collections.ArrayList
+    Import-CSV $signups | foreach-object {
+        $hash = (@{Name=$_.name;email=$_.email;match=""})
+        $row = new-object psobject -Property $hash
+        $rows.add($row) > $null
+    }
+
+    $rows = $rows | Sort-Object {get-random}
+    return $rows
 }
 
-$rows = $rows | Sort-Object {get-random}
-makePairings $rows
-clear-content output.txt
-$rows | Export-Csv output.txt
-#sendOutEmails $rows
+function CleanupWorkspace()
+{
+    clear-content output.txt
+}
+
+function Main()
+{
+    $rows = GetRandomRows
+    makePairings $rows
+    $rows | Export-Csv $LogFile
+    $cred = Get-Credential
+    sendOutEmails $rows $cred
+}
+
+Main
+
+
 
 
